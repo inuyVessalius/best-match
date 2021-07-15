11c344a54c860c6accc318fa0c3893e34fc96b45
victo <victorangelograca@hotmail.com>
2021-07-14 22:55:51 -0300

    added big file to main

--- src/main/java/bestMatch/Main.java
+++ src/main/java/bestMatch/Main.java
@@ -1,7 +1,7 @@
 package bestMatch;
 
 public class Main {
-    private static final String DICTIONARY = "small_file.txt";
+    private static final String DICTIONARY = "big_file.txt";
 
     public static void sequentialAlgorithm(String word) {
         long start = System.currentTimeMillis();
