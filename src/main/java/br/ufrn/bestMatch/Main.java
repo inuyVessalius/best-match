95f49c2fede45437841be4e5798a45763e32705a
victo <victorangelograca@hotmail.com>
2021-07-18 15:07:01 -0300

    changed to maven

---
+++ src/main/java/br/ufrn/bestMatch/Main.java
@@ -0,0 +1,27 @@
+package br.ufrn.bestMatch;
+
+public class Main {
+    private static final String DICTIONARY = "big_file.txt";
+
+    public static void sequentialAlgorithm(String word) {
+        long start = System.currentTimeMillis();
+
+        Word pair = new BestMatching().start(DICTIONARY, word);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Palavra com a menor distância: " + pair.getWord());
+
+        System.out.println("Com distância de: " + pair.getDistance());
+
+        System.out.println("Duration: "
+                + ((end - start) / 60000) + "min "
+                + (((end - start) % 60000) / 1000) + "."
+                + (((end - start) % 60000) % 1000) + "s");
+    }
+
+    public static void main(String[] args) {
+        final String word = "test";
+        sequentialAlgorithm(word);
+    }
+}
---
+++ src/main/java/br/ufrn/bestMatch/Word.java
@@ -0,0 +1,24 @@
+package br.ufrn.bestMatch;
+
+public class Word {
+    private Integer distance;
+    private String word;
+
+    public Word(Integer distance, String word) {
+        this.distance = distance;
+        this.word = word;
+    }
+
+    public void fromWord(Word other) {
+        this.distance = other.getDistance();
+        this.word = other.getWord();
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
